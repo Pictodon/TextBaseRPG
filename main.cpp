4faf881e0700edc7ec5e8afa528120997de3f66c
Pictodon <viktor887@live.com>
2016-10-07 17:43:30 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,10 @@ using namespace std;
 //Define functions
 void printString(string stringInput, bool pauseAfter);
 void pause();
+void screenResolution();
+void setTextColor(int col);
+void nameGreeting();
+void choseFontSize();
 
 //Create an array with a structure for every item
 struct items
@@ -29,111 +33,25 @@ int winWide = 0;
 //Main
 void main()
 {
-	int fontInput = 18;
-
-	top:
-
-	//Set font size
-	CONSOLE_FONT_INFOEX cfi;
-	cfi.cbSize = sizeof(cfi);
-	cfi.nFont = 0;
-	cfi.dwFontSize.X = 0;
-	cfi.dwFontSize.Y = fontInput;
-	SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
-
-	cout << "Font size: " + to_string(cfi.dwFontSize.Y) << endl;
-
-	cout << "Enter a number to change font size, and type the current font size to proceed" << endl;
-
-	cin >> fontInput;
-
-	if (fontInput == cfi.dwFontSize.Y)
-	{
-		system("cls");
-	}
-	else
-	{
-		cfi.dwFontSize.Y = fontInput;
-		system("cls");
-		goto top;
-	}
-
-	//Set text color
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
-
 	//Assign name and info for every item in the game
 	invItems[0].name = "Bottle"; invItems[0].quantity = 1; invItems[0].rarity = "Legendary";
+
+	choseFontSize();
 
 	//Set console to be fulscreen
 	SetConsoleDisplayMode(GetStdHandle(STD_OUTPUT_HANDLE), CONSOLE_FULLSCREEN_MODE, 0);
 
+	screenResolution();
+
 	//Tell the compiler what number to base rand of
 	srand(time(NULL));
+	
+	nameGreeting();
 
-	//Throw an error if you can't retrieve screen res.
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	if (!GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
-	{
-		//Error
-		cout << "Cannot determine console size." << endl;
-	}
-	else
-	{
-		//Set global vars to window size in characters
-		winHeight = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
-		winWide = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-	}
+}
 
-	//For every row in console
-	for (int i = 1; i < winHeight; i++)
-	{
-		//If it's the first row print full row with #
-		if (i == 1)
-		{
-			cout << string(winWide, '#');
-		}
-		//If it's the last row print full row with #
-		else if (i == winHeight - 1)
-		{
-			cout << string(winWide, '#');
-		//Stop the loop
-			break;
-		}
-		//For every other row print in beginning and end
-			cout << "#" << string(winWide - 2, ' ') << "#";
-	}
-
-	//Define a variable to store the input
-	int screenResInput;
-
-	//Define a goto point
-	failSaver:
-
-	cout << "Is the screen resolution above correct?\n[1] Yes\n[1] No" << endl;
-
-	cin >> screenResInput;
-
-	//Take action by input
-	switch (screenResInput)
-	{
-		//Just continue here
-		case 1:
-			system("cls");
-			break;
-		//Set Screen res to a default one 
-		case 2:
-			system("cls");
-			winHeight = 20;
-			winWide = 20;
-		//If the others didn't match go back to top and clear sceen
-		default:
-			cout << "Wrong input!" << endl;
-			//Pause and clear screen on the same line
-			pause();
-			system("cls");
-			goto failSaver;
-	}
-
+void nameGreeting()
+{
 	//Call the function to print text
 	cout << endl;
 	printString("Hello my friend, how are you?", true);
@@ -142,7 +60,7 @@ void main()
 	system("cls");
 
 	cout << endl;
-	printString("Oh that's right, I dont care.", true);
+	printString("Oh, that's right. I dont care.", true);
 
 	pause();
 	system("cls");
@@ -157,6 +75,44 @@ void main()
 	printString("Was poppin, " + cName + "?", true);
 
 	pause();
+}
+
+void choseFontSize()
+{
+	int fontInput = 18;
+
+top:
+
+	//Set font size
+	CONSOLE_FONT_INFOEX cfi;
+	cfi.cbSize = sizeof(cfi);
+	cfi.nFont = 0;
+	cfi.dwFontSize.X = 0;
+	cfi.dwFontSize.Y = fontInput;
+	SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+
+	cout << "Font size: " + to_string(cfi.dwFontSize.Y) << endl;
+
+	cout << "Enter a number to change font size,\nand type the current font size to proceed" << endl;
+
+	cin >> fontInput;
+
+	if (fontInput == cfi.dwFontSize.Y)
+	{
+		system("cls");
+	}
+	else
+	{
+		cfi.dwFontSize.Y = fontInput;
+		system("cls");
+		goto top;
+	}
+}
+
+void setTextColor(int col)
+{
+	//Set text color
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (col & 0x0F));
 }
 
 //Function to print text
@@ -194,4 +150,71 @@ void pause()
 	printString(pauseText, false);
 	cout << "> ";
 	system("pause >nul");
+}
+
+void screenResolution()
+{
+	//Throw an error if you can't retrieve screen res.
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	if (!GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+	{
+		//Error
+		cout << "Cannot determine console size." << endl;
+	}
+	else
+	{
+		//Set global vars to window size in characters
+		winHeight = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+		winWide = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+	}
+
+	//For every row in console
+	for (int i = 1; i < winHeight; i++)
+	{
+		//If it's the first row print full row with #
+		if (i == 1)
+		{
+			cout << string(winWide, '#');
+		}
+		//If it's the last row print full row with #
+		else if (i == winHeight - 1)
+		{
+			cout << string(winWide, '#');
+			//Stop the loop
+			break;
+		}
+		//For every other row print in beginning and end
+		cout << "#" << string(winWide - 2, ' ') << "#";
+	}
+
+	//Define a variable to store the input
+	int screenResInput;
+
+	//Define a goto point
+failSaver:
+
+	cout << "Is the screen resolution above correct?\n[1] Yes\n[2] No" << endl;
+
+	cin >> screenResInput;
+
+	//Take action by input
+	switch (screenResInput)
+	{
+		//Just continue here
+	case 1:
+		system("cls");
+		break;
+		//Set Screen res to a default one 
+	case 2:
+		system("cls");
+		winHeight = 20;
+		winWide = 20;
+		//If the others didn't match go back to top and clear sceen
+	default:
+		cout << "Wrong input!" << endl;
+		//Pause and clear screen on the same line
+		pause();
+		system("cls");
+		goto failSaver;
+	}
 }
