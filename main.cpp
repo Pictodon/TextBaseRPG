49a197397fa95038ae0d5aa751c4b472e2788a5e
Pictodon <viktor887@live.com>
2016-10-07 14:33:30 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include <iostream>
 #include <string>
+#include <windows.h>
 
 using namespace std;
 
@@ -12,8 +13,32 @@ struct items
 	int damage;
 } invItems[10];
 
+void printString(string String)
+{
+	for (int i = 0; i < String.length(); i++)
+	{
+		cout << String[i];
+		Sleep(70);
+	}
+	cout << endl;
+}
+
 void main()
 {
 	invItems[0].name = "Bottle"; invItems[0].quantity = 1; invItems[0].rarity = "Legendary";
 
+	printString("How dank isn't this?");
+
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	if (!GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+	{
+		// an error occourred
+		cerr << "Cannot determine console size." << endl;
+	}
+	else
+	{
+		cout << "The console is " << csbi.srWindow.Right - csbi.srWindow.Left + 1 << " wide." << endl;
+	}
+
+	system("pause");
 }
