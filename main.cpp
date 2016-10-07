cd038a6ed9c9d1092bfd459cf93c6e06baae2bb1
Pictodon <viktor887@live.com>
2016-10-07 14:18:52 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include "stdafx.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+struct items
+{
+	string name;
+	int quantity;
+	string rarity;
+	int damage;
+} invItems[10];
+
 void main()
 {
+	invItems[0].name = "Bottle"; invItems[0].quantity = 1; invItems[0].rarity = "Legendary";
 
-}  
-
+}
